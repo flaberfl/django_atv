068bc5cf3906f588d02efc10c049374ec5b562cf
Андрей Олейник <48767673+flaberfl@users.noreply.github.com>
2020-10-21 10:57:36 +0300

    1

--- src/proj/static/proj/atv-rental2.ru/js/main.js
+++ src/proj/static/proj/atv-rental2.ru/js/main.js
@@ -1,59 +1,11 @@
 $(function () {
 
-    // $("#demoTab").easyResponsiveTabs({
-    //     type: 'default', //Типы: default, vertical, accordion
-    //     width: 'auto', //auto или любое значение ширины
-    //     fit: true, // 100% пространства занимает в контейнере
-    //     activate: function () {} // Функция обратного вызова, используется, когда происходит переключение вкладок
-    // });
-
-    $('.header-slider').slick({
-        vertical: true,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 2000,
-        arrows: false,
-        centerMode: true,
-        lazyLoad: 'progressive',
-        // variableWidth: true,
-        // adaptiveHeight: true,
-        // variableWidth: true,
-        adaptiveHeight: false,
-        infinite: true,
-        responsive: [{
-                breakpoint: 992,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1,
-                    infinite: true,
-                }
-            },
-            {
-                breakpoint: 480,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1
-                }
-            }
-            // You can unslick at a given breakpoint now by adding:
-            // settings: "unslick"
-            // instead of a settings object
-        ]
+    $('.sidebar__content .sidebar__tab').on('click', function (event) {
+        var id = $(this).attr('data-id');
+        $('.category__content').find('.category__items').removeClass('active-tab').hide();
+        $('.sidebar__content .sidebar__tabs').find('.sidebar__tab').removeClass('active');
+        $(this).addClass('active');
+        $('#' + id).addClass('active-tab').fadeIn();
+        return false;
     });
-
-    // $('.info__content-description--text').readmore({
-    //     maxHeight: 240,
-    //     moreLink: '<a class="read-more" href="#">Читать полностью</a>',
-    //     lessLink: '<a class="read-more" href="#">Свернуть текст</a>'
-    // });
-
-
 });
-
-// jQuery(document).ready(function () {
-//     jQuery(".photo-list").unitegallery({
-//         gallery_theme: "tiles",
-//         tiles_type: "justified"
-//     });
-// });
