9757859e8fd8e6a81ce5967ff64daca97e146f25
Андрей Олейник <48767673+flaberfl@users.noreply.github.com>
2020-10-23 16:25:17 +0300

    застилизовал и подключил табы в моб. версии

--- src/proj/static/proj/atv-rental2.ru/js/main.js
+++ src/proj/static/proj/atv-rental2.ru/js/main.js
@@ -1,9 +1,9 @@
 $(function () {
 
-    $('.sidebar__content .sidebar__tab').on('click', function (event) {
+    $('.sidebar__content .sidebar__tab, .mobile__slider .mobile__tab').on('click', function (event) {
         var id = $(this).attr('data-id');
         $('.category__content').find('.category__items').removeClass('active-tab').hide();
-        $('.sidebar__content .sidebar__tabs').find('.sidebar__tab').removeClass('active');
+        $('.sidebar__content .sidebar__tabs, .mobile__slider .mobile__tabs').find('.sidebar__tab, .mobile__tab').removeClass('active');
         $(this).addClass('active');
         $('#' + id).addClass('active-tab').fadeIn();
         return false;
